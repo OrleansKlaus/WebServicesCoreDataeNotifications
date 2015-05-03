cba663d6f250510f51db29ea87efea6c0d848502
Matheus Amancio Seixeiro <matheusamancio23@hotmail.com>
2015-05-03 04:29:16 -0300

    arrumada as classes

--- ControleGIT/TableViewController.swift
+++ ControleGIT/TableViewController.swift
@@ -6,4 +6,106 @@
 //  Copyright (c) 2015 Orleans Klaus. All rights reserved.
 //
 
-import Foundation
+
+import UIKit
+
+class TableViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, NSURLConnectionDataDelegate, NSURLConnectionDelegate {
+    
+    // teste view 2
+    
+    var viewFoto = UIView()
+    var height = UIScreen.mainScreen().bounds.size.height/2
+    var width = UIScreen.mainScreen().bounds.size.width/2
+    var arrayProjetos = ["iDicionario", "minichallenge-1", "minichallenge -2"]
+    var usuario: String!
+    var senha: String!
+    var userData: NSDictionary!
+    var avatar: UIImage!
+    
+    
+    let searchUser: Search = Search.sharedInstance
+    @IBOutlet weak var labelNome: UILabel!
+    
+    @IBOutlet weak var myTableView: UITableView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        
+        labelNome.text = searchUser.username
+        println("lalala")
+        println(searchUser.username)
+        
+        var data = NSData(contentsOfURL: NSURL(string:searchUser.urlImage)!)
+        avatar = UIImage(data: data!)
+        self.createImage()
+        
+    }
+    
+    func createImage(){
+        let imageView = UIImageView(image: avatar)
+        imageView.frame = CGRectMake(width - 75, height - 240, 150, 150)
+        imageView.layer.masksToBounds = true
+        imageView.layer.cornerRadius = 40
+        view.addSubview(imageView)
+        
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.arrayProjetos.count;
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        var cell = myTableView.dequeueReusableCellWithIdentifier("cell", forIndexPath: indexPath) as! UITableViewCell
+        
+        cell.textLabel?.text = self.arrayProjetos[indexPath.row]
+        
+        return cell
+    }
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return "Mackmobile"
+    }
+    
+    func tableView(tableView: UITableView!, didSelectRowAtIndexPath indexPath: NSIndexPath!) {
+        println("You selected cell #\(indexPath.row)!")
+        
+    }
+    
+    
+    @IBAction func Sair(sender: AnyObject) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if let celula = sender as? UITableViewCell
+        {
+            
+            let notificacao: NSNotificationCenter = NSNotificationCenter.defaultCenter()
+            
+            notificacao.addObserver(segue.destinationViewController, selector: "criarTitulo:" , name: "novoNome", object: nil)
+            
+            
+            var nome = celula.textLabel?.text
+            let mensagem: NSDictionary = NSDictionary(object: nome!, forKey: "mensagem")
+            notificacao.postNotificationName("novoNome", object: self, userInfo: mensagem as [NSObject: AnyObject])
+            
+            println(" celula \(celula.textLabel?.text)")
+        }
+        
+    }
+    
+    /*
+    // MARK: - Navigation
+    
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+    // Get the new view controller using segue.destinationViewController.
+    // Pass the selected object to the new view controller.
+    }
+    */
+    
+}
--- ControleGIT/ViewController.swift
+++ ControleGIT/ViewController.swift
@@ -6,4 +6,106 @@
 //  Copyright (c) 2015 Orleans Klaus. All rights reserved.
 //
 
-import Foundation
+import UIKit
+import AudioToolbox
+
+class ViewController: UIViewController {
+    
+    // teste view1
+    
+    var viewFoto = UIView()
+    @IBOutlet weak var userName: UITextField!
+    @IBOutlet weak var password: UITextField!
+    @IBOutlet weak var indicador: UIActivityIndicatorView!
+    @IBOutlet weak var signInLabel: UIButton!
+    
+    var height = UIScreen.mainScreen().bounds.size.height/2
+    var width = UIScreen.mainScreen().bounds.size.width/2
+    let searchUser: Search = Search.sharedInstance
+    
+    override func viewDidLoad() {
+        
+        viewFoto.layer.frame = CGRectMake(width - 75, height - 225, 150, 150)
+        viewFoto.layer.cornerRadius = 75
+        viewFoto.layer.backgroundColor =   UIColor(patternImage: UIImage(named: "git")!).CGColor
+        viewFoto.transform = CGAffineTransformMakeRotation(CGFloat(M_PI))
+        view.addSubview(viewFoto)
+        password.secureTextEntry = true
+        
+        let notificacao: NSNotificationCenter = NSNotificationCenter.defaultCenter()
+        
+        notificacao.addObserver(self, selector: "estaPronto:" , name: "resposta", object: nil)
+        super.viewDidLoad()
+        
+        indicador.hidden = true
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func signIn(sender: AnyObject) {
+        
+        signInLabel.hidden = true
+        searchUser.user1 = userName.text
+        searchUser.senha = password.text
+        searchUser.buscaUsuario()
+        indicador.hidden = false
+        indicador.startAnimating()
+        
+        
+    }
+    
+    
+    func estaPronto(mensagem: NSNotification){
+        
+        if (searchUser.pronto == true){
+            indicador.stopAnimating()
+            NSOperationQueue.mainQueue().addOperationWithBlock {
+                self.performSegueWithIdentifier("proximo", sender: self)
+                self.userName.text = ""
+                self.password.text = ""
+                self.signInLabel.hidden = false
+                self.indicador.stopAnimating()
+                self.indicador.hidden = true
+                
+            }
+        }else{
+            AudioServicesPlaySystemSound(1352)
+            
+            ///
+            userName.text = ""
+            password.text = ""
+            signInLabel.hidden = false
+            indicador.stopAnimating()
+            indicador.hidden = true
+        }
+        
+        
+    }
+    
+    
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        
+        if (userName.becomeFirstResponder()){
+            userName.resignFirstResponder()
+            
+        }
+        if ( password.becomeFirstResponder() && userName.resignFirstResponder()){
+            password.resignFirstResponder()
+            
+        }
+        
+        return true
+    }
+    
+    override func touchesBegan(touches: Set<NSObject>, withEvent event: UIEvent) {
+        userName.resignFirstResponder()
+        password.resignFirstResponder()
+    }
+    
+    
+}
